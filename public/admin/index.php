59c18870d270e3118a9a844fc19e6b1276c8ce42
Huy Hung <nhh.216@gmail.com>
2018-03-01 18:59:58 +0700

    Update - Admin View v2

---
+++ public/admin/index.php
@@ -0,0 +1,13 @@
+<?php
+
+define('LARAVEL_START', microtime(true));
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
